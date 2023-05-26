dc8d889de832cb49bd595f4330a60a8be74169f1
oliverTuesta <oliver.jtuesta@gmail.com>
2023-05-25 22:50:05 -0500

    first commit

---
+++ components/Header/menuData.tsx
@@ -0,0 +1,17 @@
+import { Menu } from '@/types/menu'
+
+const menuData: Menu[] = [
+    {
+        id: 1,
+        title: 'Home',
+        path: '/',
+        newTab: false,
+    },
+    {
+        id: 2,
+        title: 'About',
+        path: '#hero',
+        newTab: false,
+    },
+]
+export default menuData
