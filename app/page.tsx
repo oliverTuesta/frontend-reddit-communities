0503fe37e32d5de9b09e890437eaa927c0bde06e
Oliver Tuesta <olvier.jtuesta@gmail.com>
2023-06-29 16:31:39 -0500

    feat: remove brands component

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,23 @@
-import AboutSectionOne from '@/components/About/AboutSectionOne'
-import AboutSectionTwo from '@/components/About/AboutSectionTwo'
-import Stack from '@/components/Brands'
-import ScrollUp from '@/components/Common/ScrollUp'
-import Values from '@/components/Values'
-import Hero from '@/components/Hero'
-import Team from '@/components/Team'
-import { Inter } from '@next/font/google'
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Stack from "@/components/Brands";
+import ScrollUp from "@/components/Common/ScrollUp";
+import Values from "@/components/Values";
+import Hero from "@/components/Hero";
+import Team from "@/components/Team";
+import { Inter } from "@next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    return (
-        <>
-            <ScrollUp />
-            <Hero />
-            <Values />
-            <Stack />
-            <AboutSectionOne />
-            <AboutSectionTwo />
-            <Team />
-        </>
-    )
+  return (
+    <>
+      <ScrollUp />
+      <Hero />
+      <Values />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <Team />
+    </>
+  );
 }
