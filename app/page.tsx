8ac816560be0eb0a524d93f159f4316a4af53333
Oliver Tuesta <olvier.jtuesta@gmail.com>
2023-07-14 12:00:55 -0500

    fix: brands import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Stack from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Values from "@/components/Values";
 import Hero from "@/components/Hero";
